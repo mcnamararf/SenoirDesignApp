d71efec890128d24a633770b371160699b3dbc1d
palmertj <palmertj@vcu.edu>
2019-03-04 17:28:30 -0500

    first taycommit

---
+++ app/src/main/java/com/example/mcnamararf/graphing_testone/graph_with_number.java
@@ -0,0 +1,13 @@
+package com.example.mcnamararf.graphing_testone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class graph_with_number extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph_with_number);
+    }
+}
