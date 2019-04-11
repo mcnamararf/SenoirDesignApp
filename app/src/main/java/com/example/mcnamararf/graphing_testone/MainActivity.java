8ddfea5d98683e7ba3912fecfdcb274ee5e97bbf
Tabryn <palmertj@vcu.edu>
2019-04-11 13:06:23 -0400

    Added bluetooth and glucose concentration conversion

--- app/src/main/java/com/example/mcnamararf/graphing_testone/MainActivity.java
+++ app/src/main/java/com/example/mcnamararf/graphing_testone/MainActivity.java
@@ -10,11 +10,21 @@ import com.jjoe64.graphview.series.DataPoint;
 
 import com.jjoe64.graphview.series.PointsGraphSeries;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 
 public class MainActivity extends AppCompatActivity {
     
     private Button button;
+
+    private String deviceNamePrefix = "HC-05";
+    private byte[] serial_buffer = new byte [50];
+    private byte[] input_buffer = new byte[4];
+    private double input_data;
+    private BluetoothSerial bluetoothSerial;
+    private double glucoseConcentration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +39,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        bluetoothSerial = new BluetoothSerial(
+                this, new BluetoothSerial.MessageHandler() {
+            public int read(int bufferSize, byte[] buffer) {
+                return doRead(bufferSize, buffer);
+            }
+        }, deviceNamePrefix);
+
+        if(!bluetoothSerial.connected) {
+            bluetoothSerial.connect();
+        }
+
     }
 
     public void openGraphActivity() {
         Intent intent = new Intent(this, Graph.class);
         startActivity(intent);
     }
+
+    public int doRead(int bufferSize, byte[] buffer) {
+        int i;
+        for(i = 0; i < bufferSize; i++) {
+            if(buffer[i] == 'R' && buffer[i+1] == 'O' && buffer[i+2] == 'B')
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    input_buffer[j] = buffer[i+3+j];
+                }
+            }
+        }
+
+        //input_data = toDouble(input_buffer);
+        //String input_string = Double.toString(input_data);
+        double input_double = ByteBuffer.wrap(buffer).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+        glucoseConcentration = convertToGlucose(input_double);
+        return i;
+    }
+
+    public double convertToGlucose(double voltage_in) {
+        double glucose = 0;
+        glucose = (((-8 * Math.pow(10, -5)) * voltage_in) + 1.2662);
+        return glucose;
+    }
+
 }
